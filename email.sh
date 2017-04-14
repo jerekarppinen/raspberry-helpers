2bddb2f139901fb8295ecc6f7176c400029b7c46
Jere Karppinen <jere.karppinen@mystes.fi>
2017-04-14 12:45:45 +0300

    fix

--- email.sh
+++ email.sh
@@ -1,3 +1,3 @@
-IP_ADDRESS=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo $IP_ADDRESS
-echo "$IP_ADDRESS" | mail -s "Homebot IP" email-address@gmail.com
+#IP_ADDRESS=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}$
+IP_ADDRESS=$(/sbin/ip -4 addr | grep inet | awk -F '[ \t]+|/' '{print $3}' | gr$
+echo "$IP_ADDRESS" | mail -s "Homebot IP" jere.karppinen88@gmail.com
